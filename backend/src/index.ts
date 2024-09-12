01e9c5e2eced2b584602c8894d56b69f1c80162c
spiz404 <lorespiz03@gmail.com>
2024-09-12 10:04:48 +0200

    update delete

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,24 +9,14 @@ import { imageRouter } from './images/images';
 require("dotenv").config();
 
 const app = express();
-/*
-const PORT = 8080;
-const HOST = "localhost";
-*/
-
 const router = express.Router();
+
 app.use(cors({
     credentials: true,
 }));
-/*
-const server = http.createServer(app);
-server.listen(PORT, () => {
-    console.log(`server running on http://${HOST}:${PORT}/`);
-});
-*/
 
 app.listen(process.env.BACKEND_PORT, () => {
-    console.log(`Server running on http://${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}`)
+    console.log(`Server running on http://${process.env.BACKEND_HOST}:${process.env.BACKEND_PORT}`)
 });
    
    
--- image-finder-frontend/src/SearchBar.tsx
+++ image-finder-frontend/src/SearchBar.tsx
@@ -3,10 +3,21 @@ import SearchBar from '@mkyy/mui-search-bar';
 import { useState } from "react";
 import * as React from 'react';
 import axios from 'axios';
+import { FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
 
-const Sb = ({setData}) => {
+interface imageItem {
+    url : string;
+    location : string;
+}
+
+const Sb = ({setData} : {setData : (data : Array<imageItem>) => void}) => {
 
     const [search, setSearch] = useState("");
+    const [numResults, setNumResults] = useState(1);
+
+    const handleNumResultsChange = (event : SelectChangeEvent) => {
+        setNumResults(parseInt(event.target.value));
+    };
 
     const handleSearch = () => {
 
@@ -19,16 +30,35 @@ const Sb = ({setData}) => {
             "Content-Type" : "application/json"
         }
       })
-      .then((res) => setData(res.data))
+      .then((res) => setData(res.data as Array<imageItem>))
+      .catch((err) => console.log(err));
     };
 
     return (
-      <SearchBar
-        width = '500px'
-        value={search}
-        onChange={(text : string) => setSearch(text)}
-        onSearch={handleSearch}
-      />
+      <div>
+        <SearchBar
+          width = '500px'
+          value={search}
+          onChange={(text : string) => setSearch(text)}
+          onSearch={handleSearch}
+        />
+        <FormControl color="primary" sx={{ m: 1, minWidth: 120, borderRadius: "4px", borderColor: "white" }}>
+          <InputLabel id="num-results-label" color = "info" sx={{color: "white"}} >Risultati</InputLabel>
+          <Select
+            labelId="num-results-label"
+            id="num-results-select"
+            style = {{color : "white"}}
+            value={`${numResults}`}
+            label="Risultati"
+            onChange={handleNumResultsChange}
+            sx={{ color: "white", '& .MuiOutlinedInput-notchedOutline': { borderColor: 'white' } }}
+          >
+            <MenuItem value={1}>1</MenuItem>
+            <MenuItem value={3}>3</MenuItem>
+            <MenuItem value={10}>10</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     );
 }
 
